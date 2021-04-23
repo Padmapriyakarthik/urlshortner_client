77d2ce08dfefc995ffa07b6ee65a5b61fe4049c2
PadmapriyaKarthik <padmapriyakarthik97@gmail.com>
2021-04-24 02:04:18 +0530

    UI

--- src/App/Login/index.js
+++ src/App/Login/index.js
@@ -254,7 +254,7 @@ export const Login = ({handleLogin,history})=>{
 
     }
     return(
-<div style={{backgroundImage:`url("https://mybigplunge.com/wp-content/uploads/2018/04/url-shortener-theplungedaily.jpg")`,backgroundRepeat:"no-repeat",width:"100%",height:"100%"}}>
+<div style={{backgroundImage:`url("https://mybigplunge.com/wp-content/uploads/2018/04/url-shortener-theplungedaily.jpg")`,backgroundRepeat:"no-repeat",width:"500px",height:"700px"}}>
         <div className="container center" >
             <form>
                 <h1 style={{fontSize:"28px"}}>Login</h1>
